29dea904a35d0813c66539ff08e61c820a44cc9b
ainanorjannah <ananorjannah0852@gmail.com>
2018-03-28 16:47:36 +0800

    mengisi nilai atribut

--- src/praktik01/kerudungaksi.java
+++ src/praktik01/kerudungaksi.java
@@ -12,6 +12,28 @@ package praktik01;
 public class kerudungaksi {
     public static void main(String[] args) {
         
+        Kerudung A = new Kerudung();
+        Kerudung B = new Kerudung();
+        Kerudung C = new Kerudung();
+        
+        A.bahan="satin";
+        A.harga="60.000.00";
+        A.merk="elzatta";
+        A.motip="segi_empat";
+        A.tipe_bahan="satin";
+        
+        B.bahan="kaos";
+        B.harga="90.000.00";
+        B.merk="aurora";
+        B.motip="pasmina";
+        B.tipe_bahan="kaos";
+        
+        C.bahan="katun";
+        C.harga="20.000.00";
+        C.merk="saudi";
+        C.motip="segi empat";
+        C.tipe_bahan="katun";
+        
     }
         
 }
